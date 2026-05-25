3044866cebaafcb740d4d1845363f9df85c5e0f5
Alexey Zarakovskiy <dev@azarakovskiy.com>
2026-05-25 21:03:39 +0200

    chore: drop unused migration

--- internal/tech/postgres/migrations/000002_announcer_state.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS announcer_state;
--- internal/tech/postgres/migrations/000002_announcer_state.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS announcer_state (
-    key TEXT PRIMARY KEY,
-    announced_at BIGINT NOT NULL
-);
-
-INSERT INTO announcer_state (key, announced_at)
-VALUES ('morning_summary', 0)
-ON CONFLICT (key) DO NOTHING;
